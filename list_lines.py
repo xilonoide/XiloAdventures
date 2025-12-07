bfb9c4bb680094f0bfa1ef0c5fc8e0aebe9bd34f
Sergi <sergi.pinos@outlook.com>
2025-12-07 16:52:21 +0100

    clean

--- list_lines.py
+++
@@ -1,6 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-from pathlib import Path
-text = Path('XiloAdventures.Wpf/Windows/WorldEditorWindow.xaml.cs').read_text(encoding='utf-8')
-for idx, line in enumerate(text.splitlines(), 1):
-    if idx in {63,378,430,537,681,761,781,920,934,955,964,987,1153,1176,1288,1289,1370,1424,1447,1470,1494,1522,1642,1687,1716,2084}:
-        print(idx, line.encode('unicode_escape'))
--- temp_line.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from pathlib import Path
-text = Path('XiloAdventures.Wpf/Windows/WorldEditorWindow.xaml.cs').read_text(encoding='latin1')
-print(repr(text.splitlines()[377]))
